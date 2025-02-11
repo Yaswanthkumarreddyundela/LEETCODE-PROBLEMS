27a317b7503385f331d9c52f9eccd145e05994e8
yaswanth reddy <118503416+Yaswanthkumarreddyundela@users.noreply.github.com>
2025-02-11 12:02:42 +0530

    Time: 0 ms (100%), Space: 9.5 MB (13.98%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        int length = part.size();
-        while(s.find(part) != std::string::npos){
-            int pos = s.find(part);
-            s.erase(pos,length);
+        while(s.length()>0 && s.length()>s.find(part)){
+           s.erase(s.find(part),part.length());
         }
         return s;
     }
