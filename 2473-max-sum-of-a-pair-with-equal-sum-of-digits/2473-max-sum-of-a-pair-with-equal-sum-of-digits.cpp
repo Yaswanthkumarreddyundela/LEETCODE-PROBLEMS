4076a9dbc67e77f3dd2723f1b324562314d9d193
yaswanth reddy <118503416+Yaswanthkumarreddyundela@users.noreply.github.com>
2025-02-12 12:22:28 +0530

    Time: 15 ms (83.66%), Space: 64 MB (61.17%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
     int maximumSum(vector<int>& nums) {
-        unordered_map<int,vector<int>> pair;
-        int sum=0;
-        int max1=-1;
-        for(auto i : nums){
-            pair[sumofdigits(i)].push_back(i);
-        }
-        for( auto &i : pair){
-            int n= i.second.size();
-            if (i.second.size() > 1) {
-                sort(i.second.rbegin(), i.second.rend());
-                max1 = max(max1, i.second[0] + i.second[1]);
+        unordered_map<int, pair<int, int>> pair; // Stores the top 2 max values for each digit sum
+        int max1 = -1;
+
+        for (int num : nums) {
+            int sum = sumofdigits(num);
+            
+            // Track top two largest values for each digit sum
+            if (num > pair[sum].first) {
+                pair[sum].second = pair[sum].first;
+                pair[sum].first = num;
+            } else if (num > pair[sum].second) {
+                pair[sum].second = num;
             }
         }
+
+        // Find the maximum pair sum
+        for (const auto& i : pair) {
+            if (i.second.second > 0) {  // Ensure at least 2 elements exist
+                max1 = max(max1, i.second.first + i.second.second);
+            }
+        }
+
         return max1;
-        
     }
-    int sumofdigits(int a){
-        int sum=0;
-        while(a>0){
+
+    int sumofdigits(int a) {
+        int sum = 0;
+        while (a > 0) {
             sum += a % 10;
             a /= 10;
         }
         return sum;
-
     }
 };
+
