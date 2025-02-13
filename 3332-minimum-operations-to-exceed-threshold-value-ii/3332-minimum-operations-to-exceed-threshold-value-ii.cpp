afbfd1f4f7eef9f58ce6d431f9436fc49d52784e
yaswanth reddy <118503416+Yaswanthkumarreddyundela@users.noreply.github.com>
2025-02-13 13:44:42 +0530

    Time: 101 ms (36.07%), Space: 99.2 MB (16.98%) - LeetHub

---
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        priority_queue<long long, vector<long long> , greater<long long> > pt;
+        long long count=0;
+        for(long long i:nums){
+            pt.push(i);
+        }
+        while(pt.top()<k){
+            long long x = pt.top();
+            pt.pop();
+            long long y = pt.top();
+            pt.pop();
+            pt.push(x*2+y);
+            count++;
+        }
+        return count;
+        
+    }
+};
