2e6e0d7768137f2607f4b31a8721fba2981ad933
yaswanth reddy <118503416+Yaswanthkumarreddyundela@users.noreply.github.com>
2025-02-17 13:11:23 +0530

    Time: 1 ms (88.19%), Space: 8.1 MB (76.08%) - LeetHub

---
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numTilePossibilities(string tiles) {
+        unordered_map<char, int> freq;
+        for (char ch : tiles) {
+            freq[ch]++;
+        }
+        
+        return backtrack(freq);
+    }
+
+private:
+    int backtrack(unordered_map<char, int>& freq) {
+        int count = 0;
+        for (auto& [ch, val] : freq) {
+            if (val > 0) {
+                count++;
+                freq[ch]--;
+                count += backtrack(freq);
+                freq[ch]++;
+            }
+        }
+        return count;
+    }
+};
