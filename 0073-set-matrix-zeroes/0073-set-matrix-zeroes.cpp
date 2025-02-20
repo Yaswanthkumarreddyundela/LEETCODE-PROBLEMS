fbd5a49817a3f916fb9cd0d0d42dd9c75082def2
yaswanth reddy <118503416+Yaswanthkumarreddyundela@users.noreply.github.com>
2025-02-20 20:00:16 +0530

    Time: 103 ms (3.71%), Space: 18.6 MB (52.38%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,28 +1,50 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        set<int> row;
-        set<int> col;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
+        int row = matrix.size(),col = matrix[0].size();
+        bool firstRowZero = false, firstColZero = false;
+
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 if(matrix[i][j] == 0){
-                    row.insert(i);
-                    col.insert(j);
+                    if (i == 0) firstRowZero = true;
+                    if (j == 0) firstColZero = true;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for(int r : row){
-            for(int j=0;j<matrix[r].size();j++){
-                matrix[r][j] =0;
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
             }
         }
-        for(int c : col){
-            for(int j=0;j<matrix.size();j++){
-                matrix[j][c] =0;
+        if (firstRowZero) {
+            for (int j = 0; j < col; j++) {
+                matrix[0][j] = 0;
             }
         }
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
+        if (firstColZero) {
+            for (int i = 0; i < row; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+
+        // for(int r : row){
+        //     for(int j=0;j<matrix[r].size();j++){
+        //         matrix[r][j] =0;
+        //     }
+        // }
+        // for(int c : col){
+        //     for(int j=0;j<matrix.size();j++){
+        //         matrix[j][c] =0;
+        //     }
+        // }
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 cout<<matrix[i][j]<<" ";
             }
             cout<<endl;
