6c6024bff605f5ef63715909e317d019684a54bd
yaswanth reddy <118503416+Yaswanthkumarreddyundela@users.noreply.github.com>
2025-02-27 15:12:01 +0530

    Time: 4 ms (22.43%), Space: 41.5 MB (40.35%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> answer(n, 1);
-        int prefix = 1;
-        for (int i = 0; i < n; i++) {
-            answer[i] = prefix;
-            prefix *= nums[i];
+        int count=0;
+        int mul=1;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                mul *= nums[i];
+            }
+            else{
+                count++;
+            }
         }
-        int suffix = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            answer[i] *= suffix;
-            suffix *= nums[i];
-        }
+        for(int i=0;i<nums.size();i++){
+            if(count>1){
+                ans.push_back(0);
+            }
+            else if (count == 1 && nums[i] == 0 ){
+                ans.push_back(mul);
+            }
+            else if(count == 1 && nums[i] != 0){
+                ans.push_back(0);
+            }
+            else if(count==0){
+                ans.push_back(mul/nums[i]);
+            }
 
-        return answer;
+        }
+        return ans;
     }
 };
