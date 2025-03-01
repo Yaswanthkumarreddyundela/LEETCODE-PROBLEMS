e78b14f1057294e71a389f09c0c8c97aa562ce0f
yaswanth reddy <118503416+Yaswanthkumarreddyundela@users.noreply.github.com>
2025-03-01 20:00:09 +0530

    Time: 1 ms (72.25%), Space: 11.9 MB (52.35%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char,int>mag;
-        int count;
         for(char i : magazine){
             mag[i]++;
         }
         for(char i : ransomNote){
-            if(mag.find(i) != mag.end() && mag[i] >0){
+            if(mag[i] >0){
                 mag[i]--;
-                count++;
             }
             else{
                 return false;
             }
-            
         }
-        return count == (int)ransomNote.size();
+        return true;
     }
 };
