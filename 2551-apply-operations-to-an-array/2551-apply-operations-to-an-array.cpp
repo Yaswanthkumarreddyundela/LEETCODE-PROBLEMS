ccba18a5c4ca3ce365c1b38c953f7be0d8fe7cc0
yaswanth reddy <118503416+Yaswanthkumarreddyundela@users.noreply.github.com>
2025-03-01 12:22:59 +0530

    Time: 0 ms (100%), Space: 13.3 MB (23.58%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
-        vector<int> ans;
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i] == nums[i+1]){
+        int n = nums.size();
+        
+        // Step 1: Merge adjacent equal elements
+        for(int i = 0; i < n - 1; i++) {
+            if(nums[i] == nums[i + 1]) {
                 nums[i] *= 2;
-                nums[i+1] =0;
+                nums[i + 1] = 0;
             }
         }
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=0){
-                ans.push_back(nums[i]);
+
+        // Step 2: Shift nonzero elements to the left
+        int pos = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != 0) {
+                nums[pos++] = nums[i];
             }
         }
-        int size=nums.size();
-        int size1 = ans.size();
-        for(int i=0;i<size-size1;i++){
-            ans.push_back(0);
+
+        // Step 3: Fill remaining positions with zeros
+        while(pos < n) {
+            nums[pos++] = 0;
         }
-        return ans;
+
+        return nums;
     }
 };
