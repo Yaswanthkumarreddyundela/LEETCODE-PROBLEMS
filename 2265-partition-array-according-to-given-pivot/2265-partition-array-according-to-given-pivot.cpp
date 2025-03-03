d5f9b05836bfac5ce7714ffc4e275b54367397bc
yaswanth reddy <118503416+Yaswanthkumarreddyundela@users.noreply.github.com>
2025-03-03 12:31:14 +0530

    Time: 4 ms (80.6%), Space: 127.7 MB (77.25%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        int count=0;
-        vector<int> ans;
-        for(int i : nums){
-            if(pivot>i){
-                ans.push_back(i);
-            }
-            else if(i == pivot){
-                count++;
-            }
-        }
-        if(count >0){
-            for(int i=0;i<count;i++){
-                ans.push_back(pivot);
-            }
-        }
-        for(int i : nums){
-            if(pivot<i){
-                ans.push_back(i);
-            }
-        }
-        return ans;
+        int n = nums.size();
+        vector<int> result(n); // Auxiliary array to avoid extra vector storage
+        int left = 0, right = n - 1, pivotCount = 0;
 
+        // Count pivot occurrences and place elements less than pivot
+        for (int num : nums) {
+            if (num < pivot) 
+                result[left++] = num;
+            else if (num == pivot) 
+                pivotCount++;
+        }
+
+        // Place pivot elements
+        for (int i = 0; i < pivotCount; i++) 
+            result[left++] = pivot;
+
+        // Place elements greater than pivot
+        for (int num : nums) {
+            if (num > pivot) 
+                result[left++] = num;
+        }
+
+        return result; // Return modified array
     }
 };
