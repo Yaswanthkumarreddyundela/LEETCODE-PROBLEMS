a043d8b1e2265167984086f813253d6790ae9caf
yaswanth reddy <118503416+Yaswanthkumarreddyundela@users.noreply.github.com>
2025-03-03 13:52:14 +0530

    Time: 0 ms (100%), Space: 9.1 MB (12.24%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        int min1 = INT_MAX;
+        string a = "balon";
+        int count=0;
+        int min1 = 10000000;
         unordered_map<char,int> freq;
         for(char i : text){
             freq[i]++;
         }
-        unordered_map<char, int> required = {{'b', 1}, {'a', 1}, {'l', 2}, {'o', 2}, {'n', 1}};
-        int minCount = INT_MAX; // Start with the maximum possible value
-        for (auto &[c, needed] : required) {
-            if (freq.find(c) == freq.end()) return 0; // If a required char is missing
-            minCount = min(minCount, freq[c] / needed);
+        for(char i : a){
+            if(freq[i] > 0){
+                count = freq[i];
+                if(i == 'l' || i == 'o') count/=2;
+                min1 = min(count,min1);
+            }
+            else{
+                return 0;;
+            }
         }
-        return minCount;
+        return min1;
     }
 };
