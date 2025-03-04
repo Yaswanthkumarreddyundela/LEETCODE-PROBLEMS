efe501b5c22cba351eef2ef846df8ddb6c10af69
yaswanth reddy <118503416+Yaswanthkumarreddyundela@users.noreply.github.com>
2025-03-04 14:43:51 +0530

    Time: 15 ms (73.94%), Space: 23 MB (98.73%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> dup;
+
+        for (const string& s : strs) {  // Use const reference to avoid unnecessary copying
+            string k = s;  
+            sort(k.begin(), k.end());  
+            dup[k].push_back(s);  
+        }
+
         vector<vector<string>> ans;
-        unordered_map<string,vector<string>> dup;
-        for(string s : strs){
-            string k = s;
-            sort(k.begin(),k.end());
-            dup[k].push_back(s);
+        ans.reserve(dup.size());  // Optimize memory allocation
+
+        for (auto& [key, val] : dup) {
+            ans.push_back(std::move(val));  // Move instead of copy for efficiency
         }
-        for(auto [key,val] : dup){
-            ans.push_back(val);
-        }
+
         return ans;
-        
     }
 };
