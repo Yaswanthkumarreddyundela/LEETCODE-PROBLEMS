5862c8aad2aa9edb40ec2d0ab3e04f712dfe829d
yaswanth reddy <118503416+Yaswanthkumarreddyundela@users.noreply.github.com>
2025-03-14 23:34:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -1,11 +1,37 @@
 class Solution {
 public:
-    int maximumCandies(vector<int>& A, long long k , int l = 0 , int r = 1e7) {
-        while(l < r){
-            int m = (l + r + 1)/2; long s = 0;
-            for(auto p : A) s += p/m;
-            if(s < k) r = m - 1; else l = m;
+    int maximumCandies(vector<int>& candies, long long k) {
+        int ans = 0;
+        if(sum1(candies) < k) return ans;
+        int left = 1;
+        int right = sum1(candies)/k;
+        while(left<=right){
+            int mid = (left+right)/2;
+            int res=0;
+            for(int j=0;j<candies.size();j++){
+                res += candies[j]/mid;
+            }
+            if(res == k){
+                ans=mid;
+                break;
+
+            }
+            else if( res > k){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+            
         }
-        return l;
+        return ans;
     }
+    int sum1(vector<int> & k){
+        int sum=0;
+        for(int i=0;i<k.size();i++){
+            sum+=k[i];
+        }
+        return sum;
+    }
+
 };
